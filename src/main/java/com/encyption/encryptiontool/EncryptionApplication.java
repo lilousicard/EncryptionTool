26808644b1061c8bb59c84a631c29088871fc163
Lilou Sicard-Noel <lilou.sicard-noel@sjsu.edu>
2023-08-08 14:37:15 -0700

    First GUI design

--- src/main/java/com/encyption/encryptiontool/EncryptionApplication.java
+++ src/main/java/com/encyption/encryptiontool/EncryptionApplication.java
@@ -3,16 +3,17 @@ package com.encyption.encryptiontool;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class EncryptionApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(EncryptionApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), Screen.getPrimary().getBounds().getMaxX(), Screen.getPrimary().getBounds().getMaxY());
+        stage.setTitle("The Encryptor");
         stage.setScene(scene);
         stage.show();
     }
