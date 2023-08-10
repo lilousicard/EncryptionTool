3139d25e0019f7a1fac93b3ccdfffb5085e45a53
Lilou Sicard-Noel <lilou.sicard-noel@sjsu.edu>
2023-08-10 14:10:19 -0700

    new GUI design

---
+++ src/main/java/com/encyption/encryptiontool/LoginController.java
@@ -0,0 +1,14 @@
+package com.encyption.encryptiontool;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void signInClick(ActionEvent actionEvent) {
+    }
+
+    public void signUpClick(ActionEvent actionEvent) {
+    }
+
+    public void quit(ActionEvent actionEvent) {
+    }
+}
